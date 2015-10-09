ed5c06d2866762424f2791626ef2d8592d517264
Pedro Pereira <pedro.pereira.95@hotmail.com>
2015-10-09 17:13:25 +0100

    Sphere done, 1 to go

--- code/Sphere.js
+++ code/Sphere.js
@@ -5,7 +5,9 @@
  	this.ID=id;
 
    this.radius=radius;
-   this.stacks=stacks;
+
+   if(stacks<2) this.stacks=2;
+   else this.stacks=stacks;
 
    if(slices<3) this.slices=3;
    else this.slices=slices;
@@ -30,19 +32,18 @@
   var aV=angV*deg2rad;
 
   //CALCULO DOS VERTICES E DAS NORMAIS
-  for(var j = 0; j < this.stacks+2; j++){
+  for(var j = 0; j < this.stacks+1; j++){
     for(var i = 0; i < this.slices+1; i++){
-      this.vertices.push(this.radius*Math.cos(i*aH)*Math.sin(j*aV), this.radius*Math.sin(i*aH)*Math.sin(j*aV), this.radius*Math.cos(j*aV));
+      this.vertices.push(this.radius*Math.cos(i*aH)*Math.cos(j*aV-Math.PI/2), this.radius*Math.sin(i*aH)*Math.cos(j*aV-Math.PI/2), this.radius*Math.sin(j*aV-Math.PI/2));
       this.normals.push(Math.cos(i*aH)*Math.sin(j*aV), Math.sin(i*aH)*Math.sin(j*aV), Math.cos(j*aV));
       this.texCoords.push(0, 0);
-      console.log("x- "+this.radius*Math.cos(i*aH)*Math.sin(j*aV)+" y- "+this.radius*Math.sin(i*aH)*Math.sin(j*aV)+" z- "+this.radius*Math.cos(j*aV));
     }
  }
 
  //CALCULO DOS INDICES
  for(var j = 0; j < this.stacks; j++){
    for(var i = 0; i < this.slices; i++){
-    this.indices.push(i + j * ( this.slices + 1 ), i + 1 + j * ( this.slices + 1 ), i + 1 + ( j + 1 ) * ( this.slices + 1 ));
+    this.indices.push(i + 1 + ( j + 1 ) * ( this.slices + 1 ), i + j * ( this.slices + 1 ), i + 1 + j * ( this.slices + 1 ));
     this.indices.push(i + j * ( this.slices + 1 ), i + 1 + ( j + 1 ) * ( this.slices + 1 ), i + ( j + 1 ) * ( this.slices + 1 ));
    }
  }

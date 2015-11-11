19b7a52f1c18c799c5b3a43b7188c9a22e237f9c
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2015-11-11 01:26:35 +0000

    Almost finished circular animation

--- code/CircularAnimation.js
+++ code/CircularAnimation.js
@@ -4,11 +4,24 @@ function CircularAnimation(s, c, r, sA, rA) {
      this.radius = r;
      this.startAng = sA;
      this.rotAng = rA;
+
+     /* Velocity */
+     this.velocity = this.rotAng/this.span;
 };
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
 CircularAnimation.prototype.constructor = CircularAnimation;
 
 Animation.prototype.updateMatrix = function(timePassed){
-
+  var angle = (timePassed * this.velocity + this.startAng) * Math.PI/180;
+  var x = this.radius * Math.cos(angle);
+  var z = this.radius * Math.sin(angle);
+  var transformation = {};
+  transformation.translation = [x + this.center[0], this.center[1], z + this.center[2]];
+  transformation.angle = 0;
+  return transformation;
 };
+
+var calculateAngle = function(){
+
+}

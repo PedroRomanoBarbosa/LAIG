0d78045b8556e6fcf375d2d70c1b6d7a3860d1ca
Pedro Pereira <pedro.pereira.95@hotmail.com>
2015-11-16 11:06:40 +0000

    Work 2 complete. Ugliest. Vehicle. EVER!

--- code/Vehicle.js
+++ code/Vehicle.js
@@ -13,6 +13,44 @@ function Vehicle(scene, id, headTex, bodyTex) {
 	this.dorso1 = new Triangle(this.scene, "", 2, 0, 0, 0, 1, 0, 0, 0, 4);
 	this.dorso2 = new Triangle(this.scene, "", 0, 1, 0, -2, 0, 0, 0, 0, 4);
 
+	this.rear1 = new Patch(this.scene, "", 2, 2, 10, 10, 
+	[
+		[
+			[0, 1, 0, 1],
+			[0, 3, -0.5, 1],
+			[0, 0, -1, 1]
+		],
+		[
+			[1, 0.5, 0, 1],
+			[1, 0.25, -0.5, 1],
+			[1, 0, -1, 1]
+		],
+		[
+			[2, 0, 0, 1],
+			[2, 0, -0.25, 1],
+			[2, 0, -0.5, 1]
+		]
+	]);
+
+	this.rear2 = new Patch(this.scene, "", 2, 2, 10, 10, 
+	[
+		[
+			[-2, 0, 0, 1],
+			[-2, 0, -0.25, 1],
+			[-2, 0, -0.5, 1]
+		],
+		[
+			[-1, 0.5, 0, 1],
+			[-1, 0.25, -0.5, 1],
+			[-1, 0, -1, 1]
+		],
+		[
+			[0, 1, 0, 1],
+			[0, 3, -0.5, 1],
+			[0, 0, -1, 1]
+		]
+	]);
+
 	this.dorso1.initBuffers();
 	this.dorso2.initBuffers();
 };
@@ -25,9 +63,16 @@ Vehicle.prototype.display = function() {
 	var deg2rad=Math.PI/180.0;
 
 	this.scene.pushMatrix();
+		this.scene.scale(0.2, 0.5, 0.4);
+
+	this.scene.pushMatrix();
 		this.body.bind();
         this.dorso1.display();
         this.dorso2.display();
+
+        this.head.bind();
+        this.rear1.display();
+        this.rear2.display();
     this.scene.popMatrix();
 
 	this.scene.pushMatrix();
@@ -36,5 +81,11 @@ Vehicle.prototype.display = function() {
 		this.body.bind();
         this.dorso1.display();
         this.dorso2.display();
+
+        this.head.bind();
+        this.rear1.display();
+        this.rear2.display();
+    this.scene.popMatrix();
+
     this.scene.popMatrix();
 };

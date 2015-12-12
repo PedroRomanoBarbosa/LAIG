cec5fae66b9bc6a536a562f19eb0e9f014d57bb7
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2015-12-12 00:57:40 +0000

    Added piece in the LSX

--- code2/src/XMLscene.js
+++ code2/src/XMLscene.js
@@ -167,12 +167,12 @@ XMLscene.prototype.logPicking = function (){
 				var obj = this.pickResults[i][0];
 				if (obj)
 				{
-					var customId = this.pickResults[i][1];				
+					var customId = this.pickResults[i][1];
 					console.log("Picked object: " + obj + ", with pick id " + customId);
 				}
 			}
 			this.pickResults.splice(0,this.pickResults.length);
-		}		
+		}
 	}
 }
 

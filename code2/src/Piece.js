b5798bcb4992f80612d62130cf6e5428accdf3fb
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2015-12-20 23:53:03 +0000

    Added choosing animation

--- code2/src/Piece.js
+++ code2/src/Piece.js
@@ -6,15 +6,19 @@ function Piece(scene, destination, piecePrimitive, pieceFormat, line, col) {
 
 	this.descendants = piecePrimitive.descendants;
 	this.lastTransformation = piecePrimitive.lastTransformation;
-
 	this.materialID = piecePrimitive.materialID;
-
 	this.matx = mat4.create();
-
-	this.matxAni = piecePrimitive.matxAni;
+	this.matxAni = mat4.create();
+	mat4.identity(this.matxAni);
 	this.spanSum = piecePrimitive.spanSum;
-
 	this.textureID = pieceFormat;
+
+	/* Animation related */
+	this.currentAnim = "iddle";
+	this.angleY = 0;
+	this.chosenTime = 1;
+	this.chosenVel = 180 / this.chosenTime;
+	this.aniTime = 0;
 
 	switch(destination){
 		case "board":
@@ -137,3 +141,46 @@ function Piece(scene, destination, piecePrimitive, pieceFormat, line, col) {
 	scene.objects[this.ID] = this;
 	scene.root.descendants.push(this.ID);
 };
+
+
+/**
+* Animates the piece given a time
+*/
+Piece.prototype.animate = function(time) {
+	switch (this.currentAnim) {
+		case "iddle":
+			break;
+		case "chosen":
+			this.aniTime += time;
+			var angle = (this.chosenVel*this.aniTime);
+			/* First part */
+			if(angle < 45){
+				this.angleY = -1 * angle;
+			/* Second part */
+			}else if (angle < 135) {
+				this.angleY = -90 + angle;
+			/* Third part */
+			}else if (angle < 180) {
+				this.angleY = 180 - angle;
+			}else{
+					this.angleY = 0;
+			}
+			/* reset matrix */
+			mat4.identity(this.matxAni);
+			mat4.rotate(this.matxAni, this.matxAni, this.angleY * Math.PI / 180, [0, 1, 0]);
+			/* Reset animation time */
+			if(this.aniTime > this.chosenTime){
+				this.aniTime = 0;
+			}
+		case "moving":
+			break;
+	}
+}
+
+Piece.prototype.changeAnimation = function(name){
+	if(name != "iddle" && name != "chosen" && name != "moving"){
+		console.log("The name of the animation given in change animation is not valid");
+		return false;
+	}
+	this.currentAnim = name;
+}
